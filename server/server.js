b833ee1ef66a10b9304d5b78665fea80f67fe0f6
vsenth17 <vsenth17@ford.com>
2021-08-23 13:58:22 +0530

    added connection to mongo db and removed node modules

--- server/server.js
+++ server/server.js
@@ -1,3 +1,14 @@
+const mongoose = require('mongoose')
+const Document = require("./Document")
+const defaultValue = ""
+
+mongoose.connect('mongodb://localhost/google-docs-replica',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+
 const io = require('socket.io')(3001, {
     cors: {
         origin: 'http://localhost:3000',
@@ -7,15 +18,28 @@ const io = require('socket.io')(3001, {
 
 io.on("connection", socket => {
 
-    socket.on('get-document',documentId => {
-        const data = ""
+    socket.on('get-document', async documentId => {
+        const document = await findOrCreateDocument(documentId)
         socket.join(documentId)
-        socket.emit('load-document',data)
+        socket.emit('load-document',document.data)
+         
+        socket.on('send-changes', delta => {
+            socket.broadcast.to(documentId).emit("receive-changes", delta)
+        })
+
+        socket.on('save-document', async data => {
+            console.log("printing data:"+data)
+            await Document.findByIdAndUpdate(documentId, { data })
+        })
     })
 
-    socket.on('send-changes', delta => {
-        socket.broadcast.to(documentId).emit("receive-changes", delta)
-    })
 
     console.log("connected")
 })
+
+async function findOrCreateDocument(id) {
+    if(id == null) return
+    const document = await Document.findById(id)
+    if(document) return document
+    return await Document.create({ _id: id, data: defaultValue})
+}
